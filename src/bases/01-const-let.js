094092af2158ac874ef716453280f24dde227c5b
santyxerror <sanbemal@gmail.com>
2023-08-29 19:37:00 +0200

    14. Variables y constantes

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = 'Santiago'
+const apellido = 'Benito'
+
+let valorDado = 5
+valorDado = 4
+
+console.log(nombre, apellido, valorDado)
+
+//var no se debe de usar...
+if (true) {
+  const nombre = 'Peter'
+  console.log(nombre);
+}
+
+console.log(valorDado)
