60f710dd20a4fff7efd560406b529313f8d82c82
santyxerror <sanbemal@gmail.com>
2023-08-30 20:21:45 +0200

    22. Múltiples exportaciones y exportaciones por defecto

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,25 @@
+//Import, export y funciones comunes de arreglos
+
+// import{heroes} from './data/heroes.js'
+
+import heroes, { owners } from "../data/heroes";
+
+const getHeroesById = (id) => {
+  const heroe = heroes.find(heroe => heroe.id == id)
+  return heroe
+}
+
+
+const getHeroesByOwner = (owner) => {
+  const heroe = heroes.filter(heroe => heroe.owner === owner)
+  return heroe.map(heroe => heroe.name)
+}
+
+console.log(getHeroesById(2));
+console.log(getHeroesByOwner('DC'));
+
+
+
+
+
+
