0dac310734f9121ec46694ed7efa6d85a26bb83d
santyxerror <sanbemal@gmail.com>
2023-08-31 20:36:13 +0200

    25. Async - Await

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,31 @@
+
+/* 
+const getImagenPromesa = () => new Promise(resolve => resolve('https://www.marca.com'))
+
+getImagenPromesa().then(console.log) */
+
+/* const getImage = async () => {
+  return 'www.marca.com'
+}
+
+getImage().then(console.log); */
+
+const getImage = async () => {
+
+  try{
+    const apiKey = 'sduDVwmNTRcGsZlQ6nPXAiVZAbJNCRsZ'
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+    const { data } = await resp.json()
+  
+    const { url } = data.images.original
+    const img = document.createElement('img')
+    img.src = url
+    document.body.append(img)
+
+
+  } catch(error){
+    console.error(error);
+  }
+}
+
+getImage()
